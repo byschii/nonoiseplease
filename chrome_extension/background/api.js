c0f6dd6e931ff446f08a166575fe5b8886c99c44
byschii <byschii@protonmail.com>
2024-04-17 11:03:09 +0200

    added plausible chrome extension
    not tests, will be given to users, but at their own risk

---
+++ chrome_extension/background/api.js
@@ -0,0 +1,95 @@
+
+
+
+const postRequest = (jwt, html, url, title) => {
+    // log body with shortened html
+    console.debug("body: ", {
+        html: html,
+        url: url,
+        title: title,
+    });
+
+    return {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: jwt
+        },
+        body: JSON.stringify({
+            html: html,
+            url: url,
+            title: title
+        }),
+    }
+};
+const sendPage = async (nnp_address, jwt, html, url, title) => {
+    console.log("sending page");
+    if (!jwt) {
+        console.error("no jwt");
+        return false;
+    }
+    let res = await fetch(
+        nnp_address + "/api/page-manage/load", postRequest(jwt, html, url, title)
+    ).then((response) => {
+        console.log(response.ok? "response ok" : "response not ok")
+        if (response.ok) {
+            succMsg.style.display = "block";
+            errMsg.style.display = "none";
+        }
+        return true;
+    }).catch( // return false
+        () => false
+    );
+    return res;
+};
+
+
+const refreshToken = async (nnp_address, jwt) => {
+    console.log("refreshing token");
+
+    let res = await fetch(
+        nnp_address + "/api/collections/users/auth-refresh", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: jwt
+            },
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            return null;
+        })
+
+    return res;
+};
+
+/*
+    right cannot apply filter to the search
+
+    * @param {string} nnp_address
+    * @param {string} jwt
+    * @param {string} query - search query written on the search engine
+    * @returns {Promise<[]>} with the content of the page
+*/
+const searchPageHTML = async (nnp_address, jwt, query) => {
+    console.log("searching page");
+
+    let searchParams = new URLSearchParams({
+        query: query,
+        // categories: categories.join(',')
+    })
+
+    let resp = await fetch(
+        nnp_address + "/api/search/html?"+ searchParams, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: jwt
+            }
+        });
+
+    let bodyContent = await resp.text();
+    return bodyContent;
+}
---
+++ chrome_extension/content/contentscript.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// constants and utilities
+const B = chrome;
+
+// receive message from backgroud script
+B.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("message received: ", message);
+    if(message.action === "search") {
+        // insert at top of page a list of page
+        // https://grrr.tech/posts/create-dom-node-from-html-string/
+        let resultDiv = document.createElement("div");
+        resultDiv.setHTML(message.result);
+        console.log("resultDiv: ", resultDiv);
+
+        document.body.insertBefore(
+            resultDiv,
+            document.body.firstChild
+            );
+    }
+
+    // just grab the jwt from nnp and send it background
+    // now background can do calls to backend easilly
+    if(message.action === "jwt.read") {
+        let jwtText = document.getElementById("pbjwt").textContent;
+        console.log("jwt = "+jwtText)
+        sendResponse({jwt:jwtText})
+        return;
+    }
+});
+
+
